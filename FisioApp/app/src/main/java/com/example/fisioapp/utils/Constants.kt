9b4dfb6a7b33eb986eb1a65937c8790f3c6c2b6a
freduki98 <156587594+freduki98@users.noreply.github.com>
2025-06-03 19:56:54 +0200

    Update Constants.kt

--- FisioApp/app/src/main/java/com/example/fisioapp/utils/Constants.kt
+++ FisioApp/app/src/main/java/com/example/fisioapp/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     //API NOTICIAS
     const val BASE_URL_NEWS = "https://newsapi.org/"
-    const val API_KEY = "xXx"
+    const val API_KEY = "b5b50a7fb032438ab8ae175cdbcd390a"
     const val QUERY = "fisioterapia NOT (papa OR \"Papa Francisco\" OR Vaticano)"
     const val LANGUAGE = "es"
 
